8ae9c739a25e2671d7963747d8d0436b2b4e2e27
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-19 11:43:49 -0700

    Created express server, created jsonToCSV function

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000
 
+app.use(express.static('client'));
+app.use(express.json());
 
+app.post('/upload_json', (req, res) => {
+  jsonToCSV(req.body);
+  res.end();
+})
+
+//Take Json and convert it to csv
+const jsonToCSV = (input) => {
+  let result = [];
+  let header = ['firstName', 'lastName', 'county', 'city', 'role', 'sales']
+
+  let recursion = (input) => {
+    let values = [];
+    header.forEach(key => values.push(input[key]));
+    result.push(values.join(', '));
+    input.children.forEach(child => recursion(child));
+  }
+
+  recursion(input);
+  console.log(result);
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}.`));
