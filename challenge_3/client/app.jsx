233c435b0e2bd286cd852a81185eb6d84d1c672d
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-20 14:37:19 -0700

    Set up React, Made checkout views

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,57 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      view: Checkout
+    }
+  }
 
+  changeView(component) {
+    this.setState({ view: component });
+  }
+
+  render() {
+    return this.state.view(this);
+  }
+}
+
+const Checkout = (appRef) => {
+  console.log('click');
+  return <button onClick={() => { appRef.changeView(F1) }}>Checkout</button>;
+}
+
+const F1 = (appRef) => {
+  return <>
+    <h1>F1</h1>
+
+    <form>
+      <input type="text" placeholder="First Name"/><br />
+      <input type="text" placeholder="Last Name"/><br />
+      <button type="submit" onClick={() => {appRef.changeView(F2)}}>Next</button>
+    </form>
+  </>
+}
+
+const F2 = (appRef) => {
+  return <>
+    <h1>F2</h1>
+
+    <form>
+      <input type="text" placeholder="Line 1"/><br />
+      <input type="text" placeholder="Line 2"/><br />
+      <input type="text" placeholder="City"/>
+      <input type="text" placeholder="State"/>
+      <input type="text" placeholder="Zip Code"/><br />
+      <button type="submit" onClick={() => {appRef.changeView(F3)}}>Next</button>
+    </form>
+  </>
+}
+
+const F3 = (appRef) => {
+  return <>
+    <h1>F3</h1>
+  </>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  console.log('Get was made');
+})
+
+app.post('/', (req, res) => {
+  console.log('Post was made');
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
