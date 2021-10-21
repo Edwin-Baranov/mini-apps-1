3284ac7d5eb37b36390b083611d13f0ba2538cb7
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-21 10:12:47 -0700

    App setup react/express/babel

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
+
+app.listen(port, () => {console.log(`C4 app listening at http://localhost:${port}`)})
 
